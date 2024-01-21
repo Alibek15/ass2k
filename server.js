407a4bcbb4047565f2f3c35e54a0a85f9a3be60d
Alibek15 <2018nightmare19@gmail.com>
2024-01-21 23:06:05 +0600

    aa

---
+++ server.js
@@ -0,0 +1,28 @@
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config();
+};
+
+const OPENWEATHER_API_KEY = process.env.OPENWEATHER_API_KEY;
+const axios = require('axios');
+const request = require('request');
+const express = require('express');
+const app = express();
+
+
+app.use(express.json());
+app.use(express.static('public'));
+
+app.post('/weather', (req, res) => {
+const url = `https://api.openweathermap.org/data/2.5/weather?lat=${req.body.latitude}&lon=${req.body.longitude}&appid=${OPENWEATHER_API_KEY}&units=metric`;
+axios({
+    url: url,
+    responseType: 'json'
+}).then(data => res.json(data.data));
+});
+
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log(`Server started at ${PORT}`)
+});
